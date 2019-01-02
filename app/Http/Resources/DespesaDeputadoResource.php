dd4f556711d520749ae80f01660f74e2ac451048
Alexander Leão <alexandersilvadeleao23@gmail.com>
2019-01-02 16:09:09 -0300

    Mudança de nomenclatura

--- app/Http/Resources/DespesaDeputadoResource.php
+++ app/Http/Resources/DespesaDeputadoResource.php
@@ -27,7 +27,7 @@ class DespesaDeputadoResource extends JsonResource
             'valor_documento' => $this->valor_documento,
             'mes_documento' => $this->mes_documento,
             'ano_documento' => $this->ano_documento,
-            'reacao' => ReacaoDespesaResource::collection(
+            'reacoes' => ReacaoDespesaResource::collection(
                 $this->reacaoDespesas->where('usuario_id', \Auth::user()->id)
             )
         ];
