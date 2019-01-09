18b14b71f89c8cee9a67a4dc9d50c25e15caa44d
Alexander Leão <alexandersilvadeleao23@gmail.com>
2019-01-09 10:26:26 -0300

    Nova tentativa

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Pragma" content="no-cache">
-    <meta http-equiv="Expires" content="-1">
-    <meta http-equiv="CACHE-CONTROL" content="NO-CACHE">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ asset('img/logo22.png') }}">
